bbabf3b6149ca17748bddb2f647e4f57653538da
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-11-09 06:47:18 +0530

    adding delete func.

---
+++ todoList/script.js
@@ -0,0 +1,34 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  event.preventDefault();
+  //ToDo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //create Li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // checkmark Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  // trash Button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  // append to list
+  todoList.appendChild(todoDiv);
+  // clear todo input value
+  todoInput.value = "";
+}
